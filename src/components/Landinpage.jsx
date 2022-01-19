8010725a7c66d8193eb458497db867a4d4738b31
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-19 14:38:04 +0530

    Create Landinpage.jsx

---
+++ src/components/Landinpage.jsx
@@ -0,0 +1,25 @@
+import Navbar from "../css/Navbar.css"
+
+export const Landingpage = () => {
+
+    const limg = 'https://a0.muscache.com/im/pictures/53e51dcb-8fad-4ce8-b61c-8a7a369267bf.jpg?im_w=1200'
+
+ return (
+
+    <div>
+         
+         <div style={{width:"100%" ,backgroundColor:"black"}}>
+
+         <div id='bgimg'>
+
+             <button className="text-violet-800 text-sm text-bold py-3 bg-white px-5 rounded-full" > I'm flexible</button>
+   
+             </div>
+
+         </div>
+     
+
+    </div>
+ )
+
+}
