3d4fcc8d16a19cdb11dd2b9bf7726038b6177c39
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-20 19:01:15 +0530

    Update Landinpage.jsx

--- src/components/Landinpage.jsx
+++ src/components/Landinpage.jsx
@@ -1,12 +1,13 @@
 import "../css/Landingpage.css";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Landingpage = () => {
 
-    const Place_arr = [{image:"https://a0.muscache.com/im/pictures/a433b4d0-8183-4523-b4c5-99b81c5729c1.jpg?im_" , name:"Rajasthan"} ,
+    const Place_arr = [{image:"https://a0.muscache.com/im/pictures/a433b4d0-8183-4523-b4c5-99b81c5729c1.jpg?im_" , name:"Manali"} ,
     {image:"https://a0.muscache.com/im/pictures/85bff6ba-f4d0-4094-926e-a4fbc21bab4e.jpg?im_" , name:"Hyderabad"},
-    {image:"https://a0.muscache.com/im/pictures/99eb5993-4277-4c1e-8db1-b639a96758a0.jpg?im_" , name:"Indore"},
-    {image:"https://a0.muscache.com/im/pictures/3a7b8005-28b8-48b8-8efa-0a6a00f7d5d8.jpg?im_" , name:"Bangalore"}
+    {image:"https://a0.muscache.com/im/pictures/99eb5993-4277-4c1e-8db1-b639a96758a0.jpg?im_" , name:"Pune"},
+    {image:"https://a0.muscache.com/im/pictures/3a7b8005-28b8-48b8-8efa-0a6a00f7d5d8.jpg?im_" , name:"Mumbai"}
 
 ]
 
@@ -42,11 +43,13 @@ const navigate = useNavigate();
      <div className="flex w-12/12 m-auto gap-5 mt-5">
 
          {Place_arr.map((e) => (
-
-               <div >
-               <img className="rounded-t-2xl pb-4 bg-red-600" src={e.image} alt={e.name}/>
-                <h2 className="text-2xl pt-1 pl-2 pb-20 bg-red-600 text-purple-50 rounded-b-2xl mb-10" >{e.name}</h2>
-                </div>
+            
+               <div>
+                 <Link to={`/city/${e.name}`}>
+                 <img className="rounded-t-2xl pb-4 bg-red-600" src={e.image} alt={e.name}/>
+                 <h2 className="text-2xl pt-1 pl-2 pb-20 bg-red-600 text-purple-50 rounded-b-2xl mb-10" >{e.name}</h2>
+                 </Link>
+               </div>
 
          ))}
        </div>
