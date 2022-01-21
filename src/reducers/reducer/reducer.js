643e744af72a58f72f3b3f03b317f320a967cd2a
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-21 19:45:45 +0530

    Update reducer.js

--- src/reducers/reducer/reducer.js
+++ src/reducers/reducer/reducer.js
@@ -1,10 +1,18 @@
-import { DATA } from "./actionTypes";
-let init={data:[]};
+import { DATA, PAYMENTPAGE_LOADING } from "./actionTypes";
+let init={data:[] , p_Loading:true};
 export const reducer=(state=init,{type,payload})=>{
     switch(type){
         case DATA:
             return {...state,data:payload}
-            default :
-            return state;
+           
+
+            case PAYMENTPAGE_LOADING :
+                return{
+                    ...state,
+                    p_Loading : false
+                }
+
+                default :
+                return state;
     }
 } 
