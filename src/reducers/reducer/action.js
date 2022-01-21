897fd9fd2e0b119e67adbfd612a765951596dc86
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-21 19:45:39 +0530

    Update action.js

--- src/reducers/reducer/action.js
+++ src/reducers/reducer/action.js
@@ -1,2 +1,6 @@
-import { DATA } from "./actionTypes";
+import { DATA, PAYMENTPAGE_LOADING } from "./actionTypes";
 export const  dataAction=(data)=>({type:DATA,payload:data});
+
+export const pp_Loading = () => ({
+    type : PAYMENTPAGE_LOADING,
+})
