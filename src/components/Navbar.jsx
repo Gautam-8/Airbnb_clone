2c0a965abc19c58603db48403272097d43e6dc83
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-21 19:52:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
              <Link to={'/'}> <img src={airbnb} className="w-20 h-6 mr-20px" alt=""/></Link>
         
          <div className="flex w-7/12 justify-evenly">
-           <div className="flex w-4/12 justify-center border-violet-100 border-2 border-solid rounded-full ">
+           <div className="flex w-4/12 justify-center border-grey-100 border-2 border-solid rounded-full ">
 
 
            <input autocomplete="off" type='text' placeholder="Start your search here" className="rounded-full text-xs text-black text-semibold w-12/12 h-7 mr-1 focus:outline-none"
@@ -90,7 +90,7 @@ export const Navbar = () => {
          <path d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"></path>
          </svg>
 
-       <button className="ml-4 border-2 border-solid border-indigo-50 rounded-full px-2 py-1">
+       <button className="ml-4 border-2 border-solid border-grey-200 rounded-full px-2 py-1 shadow-slate-400">
 
         <div className="flex items-center">
 
