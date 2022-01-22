0201c6649fd7e529052b15a05ebba6dee0d51857
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-22 20:21:12 +0530

    Update actionTypes.js

--- src/reducers/reducer/actionTypes.js
+++ src/reducers/reducer/actionTypes.js
@@ -4,3 +4,4 @@ export const PAYMENTPAGE_LOADING = 'PAYMENTPAGE_LOADING';
 export const PAYMENT_REQ = 'PAYMENT_REQ';
 export const PAYMENT_SUCCESS = 'PAYMENT_SUCCESS';
 export const PAYMENT_ERROR = 'PAYMENT_ERROR';
+export const ORDER = 'ORDER';
