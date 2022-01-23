a06e6aa475f1a95c78686e8808dd0e25439855ca
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-23 10:13:38 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -50,7 +50,7 @@ export const Login=()=>{
         let currentusername=res.username;
         console.log(currentusername);
         localStorage.setItem("token",JSON.stringify(currentusername));
-        navigate(-1)
+        navigate('/')
     })
     .catch((e)=>{
         console.log(e);
